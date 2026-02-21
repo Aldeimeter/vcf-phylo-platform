f5a5b0dfb68f350fbc45b77932c45f249ed309ea
Artem Zaitsev <Aldeimeter666@gmail.com>
2026-02-21 22:28:48 +0100

    feat(server): add CORS

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,19 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from .routers import datasets, jobs
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount(
     "/static/results", StaticFiles(directory=os.environ["STATIC_PATH"]), name="results"
